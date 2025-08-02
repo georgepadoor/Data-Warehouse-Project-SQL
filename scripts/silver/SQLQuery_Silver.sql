4446c86caa52636fe908fa18670c99e6cb72e032
georgepadoor <georgepadoor@gmail.com>
2025-08-02 23:58:03 +0530

    load crm_cust_info to silver layer

--- scripts/silver/SQLQuery_Silver.sql
+++ scripts/silver/SQLQuery_Silver.sql
@@ -4,3 +4,130 @@ SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[crm_sales_details]
 SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[erp_cust_az12]
 SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[erp_loc_a101]
 SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[erp_px_cat_g1v2]
+
+SELECT TOP (1000) * FROM [DataWarehouse].[silver].[crm_cust_info]
+
+-- checks for null or duplicates in primary key
+
+SELECT
+cst_id,
+count(*) 
+FROM bronze.crm_cust_info
+group by cst_id
+having count(*) > 1 or cst_id is null
+
+
+SELECT
+* ,
+row_number () over (partition by cst_id order by cst_create_date desc) as rank_latest
+FROM bronze.crm_cust_info
+where cst_id = 29466
+
+
+
+select
+*
+from
+
+(SELECT
+* ,
+row_number () over (partition by cst_id order by cst_create_date desc) as rank_latest
+FROM bronze.crm_cust_info) t
+where rank_latest = 1 and cst_id = 29466
+
+select
+*
+from
+
+(SELECT
+* ,
+row_number () over (partition by cst_id order by cst_create_date desc) as rank_latest
+FROM bronze.crm_cust_info
+where cst_id is not null
+) t
+where rank_latest = 1
+
+
+
+
+-- check for unwanted spaces
+
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+where cst_firstname != trim(cst_firstname)
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+where cst_lastname != trim(cst_lastname)
+
+SELECT cst_gndr
+FROM bronze.crm_cust_info
+where cst_gndr != trim(cst_gndr)
+
+
+-- data standardization and consistency
+select distinct cst_gndr
+FROM bronze.crm_cust_info
+
+-- after loading silver doing quality checks
+
+SELECT TOP (1000) * FROM [DataWarehouse].[silver].[crm_cust_info]
+
+-- checks for null or duplicates in primary key
+
+SELECT
+cst_id,
+count(*) 
+FROM silver.crm_cust_info
+group by cst_id
+having count(*) > 1 or cst_id is null
+
+-- check for unwanted spaces
+
+SELECT cst_firstname
+FROM silver.crm_cust_info
+where cst_firstname != trim(cst_firstname)
+
+
+SELECT cst_lastname
+FROM silver.crm_cust_info
+where cst_lastname != trim(cst_lastname)
+
+SELECT cst_gndr
+FROM silver.crm_cust_info
+where cst_gndr != trim(cst_gndr)
+
+-- data standardization and consistency
+select distinct cst_gndr
+FROM silver.crm_cust_info
+
+--clean and load crm_prd_info
+
+SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[crm_prd_info]
+
+SELECT
+prd_id,
+count(*) 
+FROM bronze.crm_prd_info
+group by prd_id
+having count(*) > 1 or prd_id is null
+
+
+SELECT
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+FROM bronze.crm_prd_info
+where REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') not in 
+(select distinct id from bronze.erp_px_cat_g1v2)
+
+SELECT
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+LEN(prd_key) AS len_prd_key   
+FROM bronze.crm_prd_info
+
+
+SELECT
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+FROM bronze.crm_prd_info
+
+where REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') not in 
+(select distinct id from bronze.erp_px_cat_g1v2)
---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,67 @@
+TRUNCATE TABLE silver.crm_cust_info;
+INSERT INTO silver.crm_cust_info (
+			cst_id, 
+			cst_key, 
+			cst_firstname, 
+			cst_lastname, 
+			cst_marital_status, 
+			cst_gndr,
+			cst_create_date)
+           
+select
+    cst_id,
+    cst_key,
+    trim (cst_firstname) as cst_firstname,
+    trim(cst_lastname) as cst_lastname,
+    case
+        when upper(trim(cst_marital_status)) = 'S' THEN 'Single'
+        when upper(trim(cst_marital_status)) = 'M' THEN 'Married'
+        else 'n/a'
+    end as cst_marital_status, -- Normalize marital status values to readable format
+    case 
+        WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+				ELSE 'n/a'
+			END AS cst_gndr, -- Normalize gender values to readable format
+    cst_create_date
+from
+
+(SELECT
+* ,
+row_number () over (partition by cst_id order by cst_create_date desc) as rank_latest
+FROM bronze.crm_cust_info
+where cst_id is not null
+) t
+where rank_latest = 1
+
+
+
+INSERT INTO silver.crm_prd_info (
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt
+		)
+		SELECT
+			prd_id,
+			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
+			prd_nm,
+			ISNULL(prd_cost, 0) AS prd_cost,
+			CASE 
+				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+				ELSE 'n/a'
+			END AS prd_line, -- Map product line codes to descriptive values
+			CAST(prd_start_dt AS DATE) AS prd_start_dt,
+			CAST(
+				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
+				AS DATE
+			) AS prd_end_dt -- Calculate end date as one day before the next start date
+		FROM bronze.crm_prd_info
