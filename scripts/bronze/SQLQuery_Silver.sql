033c9c9579d308b745824fa8a27b8ed9a5691f1f
georgepadoor <georgepadoor@gmail.com>
2025-08-02 20:08:59 +0530

    sql quering for silver layer

---
+++ scripts/bronze/SQLQuery_Silver.sql
@@ -0,0 +1,6 @@
+SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[crm_cust_info]
+SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[crm_prd_info]
+SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[crm_sales_details]
+SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[erp_cust_az12]
+SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[erp_loc_a101]
+SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[erp_px_cat_g1v2]
