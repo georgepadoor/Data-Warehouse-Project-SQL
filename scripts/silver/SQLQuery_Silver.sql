4e0925d1f17580a22a806cf0f0aa68b609f1f6ac
georgepadoor <georgepadoor@gmail.com>
2025-08-10 12:27:58 +0530

    added crminfo to silver layer

--- scripts/silver/SQLQuery_Silver.sql
+++ scripts/silver/SQLQuery_Silver.sql
@@ -114,7 +114,7 @@ having count(*) > 1 or prd_id is null
 
 
 SELECT
-REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id
 FROM bronze.crm_prd_info
 where REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') not in 
 (select distinct id from bronze.erp_px_cat_g1v2)
@@ -126,8 +126,184 @@ FROM bronze.crm_prd_info
 
 
 SELECT
-SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key
 FROM bronze.crm_prd_info
 
 where REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') not in 
 (select distinct id from bronze.erp_px_cat_g1v2)
+
+select sls_prd_key from bronze.crm_sales_details
+
+SELECT
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key
+FROM bronze.crm_prd_info
+where SUBSTRING(prd_key, 7, LEN(prd_key)) not in (
+select sls_prd_key from bronze.crm_sales_details)
+
+
+select sls_prd_key 
+from bronze.crm_sales_details 
+where sls_prd_key like 'FK-1639'
+
+
+select sls_prd_key 
+from bronze.crm_sales_details 
+where sls_prd_key like 'FK-1%'
+
+
+SELECT
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key
+FROM bronze.crm_prd_info
+where SUBSTRING(prd_key, 7, LEN(prd_key)) in (
+select sls_prd_key from bronze.crm_sales_details)
+
+-- check for unwanted spaces
+
+SELECT prd_nm
+FROM bronze.crm_prd_info
+where prd_nm != trim(prd_nm)
+
+-- check for nulls or negative numbers
+
+SELECT prd_cost
+FROM bronze.crm_prd_info
+where prd_cost  < 0 or prd_cost is null
+
+-- data standardization and consistency
+
+SELECT distinct prd_line
+FROM bronze.crm_prd_info
+
+
+
+-- check for invalid date orders
+
+SELECT *
+FROM bronze.crm_prd_info
+where prd_end_dt < prd_start_dt
+
+-- creating new end date
+SELECT *
+FROM bronze.crm_prd_info
+where prd_key in('AC-HE-HL-U509-R','AC-HE-HL-U509')
+
+
+SELECT *,
+lead(prd_start_dt) over (partition by prd_key order by prd_start_dt)-1 AS prd_end_dt_test
+FROM bronze.crm_prd_info
+
+where prd_key in('AC-HE-HL-U509-R','AC-HE-HL-U509')
+
+
+SELECT *,
+CAST(prd_start_dt AS DATE) AS prd_start_dt,
+CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
+				AS DATE
+			) AS prd_end_dt
+FROM bronze.crm_prd_info
+
+-- after loading silver layer quality checks
+SELECT
+prd_id,
+count(*) 
+FROM silver.crm_prd_info
+group by prd_id
+having count(*) > 1 or prd_id is null
+
+-- check for unwanted spaces
+
+SELECT prd_nm
+FROM silver.crm_prd_info
+where prd_nm != trim(prd_nm)
+
+-- check for nulls or negative numbers
+
+SELECT prd_cost
+FROM silver.crm_prd_info
+where prd_cost  < 0 or prd_cost is null
+
+-- data standardization and consistency
+
+SELECT distinct prd_line
+FROM silver.crm_prd_info
+
+
+-- check for invalid date orders
+
+SELECT *
+FROM silver.crm_prd_info
+where prd_end_dt < prd_start_dt
+
+
+SELECT *
+FROM silver.crm_prd_info
+
+-- clean and load crm_sales_details
+SELECT *
+FROM bronze.crm_sales_details
+where sls_ord_num !=trim(sls_ord_num)
+
+SELECT *
+FROM bronze.crm_sales_details
+where sls_prd_key not in (select prd_key from silver.crm_prd_info)
+
+SELECT *
+FROM bronze.crm_sales_details
+where sls_cust_id not in (select cst_id from silver.crm_cust_info)
+
+SELECT *
+FROM bronze.crm_sales_details
+where sls_order_dt <= 0
+
+SELECT 
+nullif(sls_order_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+where sls_order_dt <= 0  or len(sls_order_dt)!=8 or sls_order_dt > 20300101
+
+SELECT 
+nullif(sls_order_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+where sls_order_dt > 20300101 or sls_order_dt < 20000101
+
+SELECT 
+nullif(sls_ship_dt,0) sls_ship_dt
+FROM bronze.crm_sales_details
+where sls_ship_dt <= 0  or len(sls_ship_dt)!=8 or sls_ship_dt > 20300101
+
+
+SELECT 
+nullif(sls_due_dt,0) sls_due_dt
+FROM bronze.crm_sales_details
+where sls_due_dt <= 0  or len(sls_due_dt)!=8 or sls_due_dt > 20300101
+
+select *
+FROM bronze.crm_sales_details
+where sls_order_dt > sls_ship_dt or sls_order_dt > sls_due_dt
+
+select *
+FROM bronze.crm_sales_details
+where sls_ship_dt > sls_due_dt
+
+select *
+FROM bronze.crm_sales_details
+where sls_sales != sls_quantity * sls_price
+
+select *
+FROM bronze.crm_sales_details
+where sls_sales <= 0 or sls_quantity <= 0 or sls_price <= 0
+order by  sls_sales, sls_quantity, sls_price 
+
+
+select *
+FROM bronze.crm_sales_details
+where sls_sales is null or sls_quantity is null or sls_price is null
+order by  sls_sales, sls_quantity, sls_price 
+
+
+
+
+
+
+
+
+
--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -32,10 +32,10 @@ row_number () over (partition by cst_id order by cst_create_date desc) as rank_l
 FROM bronze.crm_cust_info
 where cst_id is not null
 ) t
-where rank_latest = 1
+where rank_latest = 1  -- Select the most recent record per customer
 
 
-
+TRUNCATE TABLE silver.crm_prd_info;
 INSERT INTO silver.crm_prd_info (
 			prd_id,
 			cat_id,
@@ -52,16 +52,57 @@ INSERT INTO silver.crm_prd_info (
 			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
 			prd_nm,
 			ISNULL(prd_cost, 0) AS prd_cost,
-			CASE 
-				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
-				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
-				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
-				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+			CASE  UPPER(TRIM(prd_line))
+				WHEN 'M' THEN 'Mountain'
+				WHEN 'R' THEN 'Road'
+				WHEN 'S' THEN 'Other Sales'
+				WHEN 'T' THEN 'Touring'
 				ELSE 'n/a'
 			END AS prd_line, -- Map product line codes to descriptive values
 			CAST(prd_start_dt AS DATE) AS prd_start_dt,
-			CAST(
-				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
-				AS DATE
-			) AS prd_end_dt -- Calculate end date as one day before the next start date
+			CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) 
+			AS prd_end_dt -- Calculate end date as one day before the next start date
 		FROM bronze.crm_prd_info
+
+
+	
+TRUNCATE TABLE silver.crm_sales_details;
+INSERT INTO silver.crm_sales_details (
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
+		)
+		SELECT 
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			CASE 
+				WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+			END AS sls_order_dt,
+			CASE 
+				WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+			END AS sls_ship_dt,
+			CASE 
+				WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+			END AS sls_due_dt,
+			CASE 
+				WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+					THEN sls_quantity * ABS(sls_price)
+				ELSE sls_sales
+			END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+			sls_quantity,
+			CASE 
+				WHEN sls_price IS NULL OR sls_price <= 0 
+					THEN sls_sales / NULLIF(sls_quantity, 0)
+				ELSE sls_price  -- Derive price if original value is invalid
+			END AS sls_price
+		FROM bronze.crm_sales_details
